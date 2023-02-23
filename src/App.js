228d36bdf6f3ad78ff42123eb0418050c99d3fa8
Gigi Asatiani <123gigiasatiani123@gmail.com>
2023-02-23 13:09:34 +0400

    additional features

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import FormRegistration from "./components/formRegistration";
+import { Home } from "./components/Home";
 import "./App.css";
 
 function App() {
+  const [page, setPage] = useState(1);
+
+  function handleNextPage() {
+    setPage(2);
+  }
+
   return (
     <div className="App">
-      <FormRegistration />
+      {page === 1 ? (
+        <Home handleNextPage={handleNextPage} />
+      ) : (
+        <FormRegistration />
+      )}
     </div>
   );
 }
